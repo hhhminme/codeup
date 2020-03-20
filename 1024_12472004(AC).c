6f4b0e8ef0e4c8f80190eb522aab956027f41358
kenma-dev <kenma409@gmail.com>
2020-03-20 20:26:40 +0900

    soucefiles

---
+++ 1024_12472004(AC).c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int main() {
+	char string[20];
+	int i;
+
+	gets(string);
+	for (i = 0; i < sizeof(string); i++) {
+		if (string[i] == NULL) 
+			break;
+
+		printf("'%c'\n", string[i]);
+	}
+}
---
+++ 1027_12473526(AC).c
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	int y, m, d;
+	scanf("%d.%d.%d", &y, &m, &d);
+	printf("%02d-%02d-%d", d, m, y);
+	return 0;
+}
